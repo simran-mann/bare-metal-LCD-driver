c55240df9dd44a0d99093790c81ded23f6f19bb3
simran-mann <simranmann2004@gmail.com>
2025-05-07 22:47:00 -0700

    adding the files

---
+++ EmbeddedGraphicsLVGL-MCU3-main/Projects/bare_metal_rainbow/3_32F407DISCOVERY/Debug/Src/main.d
@@ -0,0 +1,9 @@
+Src/main.o: ../Src/main.c ../Inc/bsp_lcd.h ../Inc/arm_cm4.h \
+ ../Inc/stm32f407xx.h ../Inc/reg_util.h ../Inc/ili9341_reg.h \
+ ../Inc/board.h
+../Inc/bsp_lcd.h:
+../Inc/arm_cm4.h:
+../Inc/stm32f407xx.h:
+../Inc/reg_util.h:
+../Inc/ili9341_reg.h:
+../Inc/board.h:
